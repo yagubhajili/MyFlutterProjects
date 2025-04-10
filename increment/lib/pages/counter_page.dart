6726122db3c544fd5264ad9e7c4251d3d5411138
yagubhajili <yaqubhacili6@gmail.com>
2025-04-10 18:35:47 +0400

    increment app setstate

---
+++ increment/lib/pages/counter_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CounterPage extends StatefulWidget {
+  const CounterPage({super.key});
+
+  @override
+  State<CounterPage> createState() => _CounterPageState();
+}
+
+class _CounterPageState extends State<CounterPage> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('click this many times: '),
+            Text(counter.toString(), style: TextStyle(fontSize: 35)),
+            ElevatedButton(onPressed: increment, child: Text('click')),
+          ],
+        ),
+      ),
+    );
+  }
+}
