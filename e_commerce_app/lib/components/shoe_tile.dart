3199bfdc829c33313a5eb4928ba59c2dd27273b9
yagubhajili <yaqubhacili6@gmail.com>
2025-05-22 00:34:11 +0400

    shop page half

---
+++ e_commerce_app/lib/components/shoe_tile.dart
@@ -0,0 +1,71 @@
+import 'package:e_commerce_app/models/shoe.dart';
+import 'package:flutter/material.dart';
+
+class ShoeTile extends StatelessWidget {
+  Shoe shoe;
+  ShoeTile({super.key, required this.shoe});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      width: 280,
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          //pic
+          ClipRRect(
+            borderRadius: BorderRadius.circular(12),
+            child: Image.asset(shoe.imagePath, fit: BoxFit.cover),
+          ),
+          //desc
+          Text(shoe.description, style: TextStyle(color: Colors.grey[600])),
+          //price
+          Padding(
+            padding: const EdgeInsets.only(left: 25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //name
+                    Text(
+                      shoe.name,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+
+                    //price
+                    Text('\$' + shoe.price),
+                  ],
+                ),
+
+                //plus button
+                Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(12),
+                      bottomRight: Radius.circular(12),
+                    ),
+                  ),
+                  child: Icon(Icons.add, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+          //button
+        ],
+      ),
+    );
+  }
+}
--- e_commerce_app/lib/pages/shop_page.dart
+++ e_commerce_app/lib/pages/shop_page.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/components/shoe_tile.dart';
+import 'package:e_commerce_app/models/shoe.dart';
 import 'package:flutter/material.dart';
 
 class ShopPage extends StatefulWidget {
@@ -13,10 +15,71 @@ class _ShopPageState extends State<ShopPage> {
     return Column(
       children: [
         //search bar
-
+        Container(
+          padding: EdgeInsets.all(12),
+          margin: EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('search', style: TextStyle(color: Colors.grey)),
+              Icon(Icons.search, color: Colors.grey),
+            ],
+          ),
+        ),
         //message
+        Padding(
+          padding: EdgeInsets.symmetric(vertical: 25),
+          child: Text(
+            'everyone flies.. some fly longer than others',
+            style: TextStyle(color: Colors.grey[600]),
+          ),
+        ),
 
         //shoes
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Hot picks',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
+              ),
+              Text(
+                'see all',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(height: 10),
+
+        Expanded(
+          child: ListView.builder(
+            itemCount: 3,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              Shoe shoe = Shoe(
+                name: 'Jordan',
+                price: '55',
+                imagePath: 'lib/images/pic1.webp',
+                description: 'cool shoe',
+              );
+              return ShoeTile(shoe: shoe);
+            },
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 25, left: 25, right: 25),
+          child: Divider(color: Colors.white),
+        ),
       ],
     );
   }
