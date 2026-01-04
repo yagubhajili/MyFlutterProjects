b8558dcc3752a501ab4c1042c9da63ee7366612c
yagubhajili <yaqubhacili6@gmail.com>
2026-01-04 20:44:04 +0400

    started to create onboarding page

---
+++ onboardingapp/android/app/src/main/kotlin/com/example/onboardingapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.onboardingapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ onboardingapp/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(OnBoardingApp());
+}
+
+class OnBoardingApp extends StatelessWidget {
+  const OnBoardingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Container(height: 10)));
+  }
+}
